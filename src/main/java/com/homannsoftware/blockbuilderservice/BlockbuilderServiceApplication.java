8c44fb2c3cba14a1dee6292413bec73594eb1be5
Enrico Homann <enrico.homann@homannsoftware.com>
2022-10-26 14:25:45 +0200

    Initial

---
+++ src/main/java/com/homannsoftware/blockbuilderservice/BlockbuilderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.homannsoftware.blockbuilderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockbuilderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockbuilderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/homannsoftware/blockbuilderservice/BlockbuilderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homannsoftware.blockbuilderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockbuilderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
